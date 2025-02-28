41f682f63038fff3daee0094e72ced520d88bf05
CamiloCT3105 <Usuario@B6-603-37781>
2025-02-28 13:10:34 -0500

    Agregar archivos de proyecto.

---
+++ Aplicacion2/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System.Windows.Forms;
+
+namespace Aplicacion2
+{
+    public partial class PRIMER_FORM : Form
+    {
+        public PRIMER_FORM()
+        {
+            InitializeComponent();
+        }
+        private void TxtInfo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13 || e.KeyChar == 32)
+            {
+                MessageBox.Show("PRESIONASTE ENTER");
+                txt_Nombre.Focus();
+            }
+            if (e.KeyChar >= 48 && e.KeyChar <= 57 || e.KeyChar == 8)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txt_Nombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13 || e.KeyChar == 32)
+            {
+                Txt_NUMERO.Focus();
+            }
+            if (e.KeyChar < 48 || e.KeyChar > 57)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Txt_NUMERO_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13 || e.KeyChar == 32)
+            {
+                TXT_CORREO.Focus();
+            }
+            if (e.KeyChar >= 48 && e.KeyChar <= 57 || e.KeyChar == 8)
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TXT_CORREO_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13 || e.KeyChar == 32)
+            {
+                TXT_CORREO.Focus();
+            }
+        }
+    }
+}
